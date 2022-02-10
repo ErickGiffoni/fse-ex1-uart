b4cb26566ae58be9fc25d8421eea006dbf0ec3f0
ErickGiffoni <giffoni.erick@gmail.com>
2022-02-10 14:47:39 -0300

    usa o modulo da UART

--- src/uart-rasp.c
+++ src/uart-rasp.c
@@ -10,77 +10,17 @@
  * 
  */
 
-#include <fcntl.h>      // open
-#include <termios.h>    // tcgetattr, struct termios...
-#include <stdlib.h>     // malloc
-#include <unistd.h>     // write
-#include <string.h>     // strncpy
-#include <stdio.h>
-
-#define SEND_STRING_CODE   0xB3
-
-void openUart(int *descriptor, char *path);
-void setCommunicationOptions(struct termios *commOptions, int *descriptor);
-
-void sendString(char *message, int msgLength, int *descriptor);
+#include <termios.h>    // struct termios
+#include "uart.h"
 
 int main(){
 
-   int uartDescriptor = -1;
-   openUart(&uartDescriptor, "/dev/serial0");
+   openUart("/dev/serial0");
 
    struct termios commOptions;
-   setCommunicationOptions(&commOptions, &uartDescriptor);
+   setCommunicationOptions(&commOptions);
 
-   sendString("Eu sou eu", 9, &uartDescriptor);
+   sendString("Eu sou eu", 9);
 
    return 0;
 } // end of main
-
-void openUart(int *descriptor, char *path){
-   *descriptor = open(path, O_RDWR | O_NOCTTY | O_NDELAY);
-
-   if(*descriptor == -1){
-      printf("Error openning UART");
-      exit(1);
-   }
-
-   return;
-} // end of openUart
-
-/**
- * Access UART hardware and sets its configurations
- */
-void setCommunicationOptions(struct termios *commOptions, int *descriptor){
-   tcgetattr(*descriptor, commOptions);
-
-   commOptions->c_cflag = B9600 | CS8 | CLOCAL | CREAD;  //<Set baud rate
-   commOptions->c_iflag = IGNPAR;
-   commOptions->c_oflag = 0;
-   commOptions->c_lflag = 0;
-
-   tcflush(*descriptor, TCIFLUSH);
-   tcsetattr(*descriptor, TCSANOW, commOptions);
-
-   return;
-} // end of setCommunicationOptions
-
-void sendString(char *message, int msgLength, int *descriptor){
-   int pkgLength = 2+msgLength+4;
-   char *package = (char *) malloc(pkgLength * sizeof(char));
-
-   package[0]  =  SEND_STRING_CODE;
-   package[1]  =  msgLength;
-
-   strncpy(package+2, message, msgLength);
-
-   package[pkgLength-4] =  1;
-   package[pkgLength-3] =  1;
-   package[pkgLength-2] =  6;
-   package[pkgLength-1] =  1;
-
-   int numOfBytesWritten = 0;
-   numOfBytesWritten = write(*descriptor, &package[0], pkgLength);
-
-   return;
-} // end of sendString
